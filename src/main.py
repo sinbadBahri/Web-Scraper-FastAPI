3325cfb643ce85b228f32b2f3fb95b8f179ea5bb
sinbadBahri <h.l.kh7990@gmail.com>
2024-01-09 15:25:00 +0330

    New POST Endpoint added

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+import fastapi as _fastapi
 from fastapi import responses as _responses
 
-app = FastAPI()
+app = _fastapi.FastAPI()
 
 import services as _services
 
@@ -11,7 +11,12 @@ async def root():
     return {'message': 'Welcome to my meme api'}
 
 
-@app.get('/general-memes')
-async def get_general_memes():
+@app.get('/programmer-memes')
+def get_programmer_memes():
     image_path = _services.select_random_image("ProgrammerHumor")
     return _responses.FileResponse(image_path)
+
+
+@app.post('/programmer-memes')
+def create_programmer_meme(image: _fastapi.UploadFile = _fastapi.File(...)):
+    pass
