bd986fdb6a2e5103777ee92f1569457b80dfa770
sinbadBahri <h.l.kh7990@gmail.com>
2024-01-09 13:12:45 +0330

    Branch Final Commit: Core Function "_collect_memes" defined, along with some small changes in other functions. Collector has been Tested Successfully :)

--- src/collector.py
+++ src/collector.py
@@ -39,7 +39,7 @@ def _get_image_urls(client: _praw.Reddit, subreddit_name: str, limit: int):
 
 def _get_image_name(image_url: str) -> str:
     image_name = _parse.urlparse(image_url)
-    return _os.path.basename(image_name)
+    return _os.path.basename(image_name.path)
 
 
 def _create_folder(folder_name: str):
@@ -50,41 +50,45 @@ def _create_folder(folder_name: str):
     try:
         _os.mkdir(folder_name)
     except OSError:
-        print("OOPS; Something went wrong !!")
+        print("Using The Exiting Folder ...")
     else:
-        print("Folder created")
+        print("New Folder Created.")
 
 
 def _download_image(folder_name: str, raw_response, image_name: str):
+    """
+    Downloads and saves the images in the folder, which has already been created.
+    """
     _create_folder(folder_name=folder_name)
+
     with open(f"{folder_name}/{image_name}", 'wb') as image_file:
         _shutil.copyfileobj(raw_response, image_file)
 
 
-# def _collect_memes(subreddit_name: str, limit: int=20):
-#     """
-#     Collect the images from the URLs and stores them into
-#     the folders, named after their subreddits
-#     """
-#     client = _create_reddit_client()
-#     image_urls = _get_image_urls(
-#         client=client,
-#         subreddit_name=subreddit_name,
-#         limit=limit
-#     )
+def _collect_memes(subreddit_name: str, limit: int=20):
+    """
+    Collect the images from the URLs and stores them into
+    the folders, named after their subreddits
+    """
+    client = _create_reddit_client()
+    image_urls = _get_image_urls(
+        client=client,
+        subreddit_name=subreddit_name,
+        limit=limit
+    )
 
-#     for image_url in image_urls:
-#         image_name = _get_image_name(image_url=image_url)
-#         response = _requests.get(image_url, stream=True)
+    for image_url in image_urls:
+        image_name = _get_image_name(image_url=image_url)
+        response = _requests.get(image_url, stream=True)
 
-#         if response.status_code == 200:
-#             response.raw.decode_content = True
-#             _download_image()
-
-
+        if response.status_code == 200:
+            response.raw.decode_content = True
+            _download_image(
+                folder_name=subreddit_name,
+                raw_response=response.raw,
+                image_name=image_name
+            )
 
 
 if __name__ == '__main__':
-    client = _create_reddit_client()
-    urls = _get_image_url(client=client, subreddit_name="memes", limit=10)
-    print(urls)
+    _collect_memes("memes")
