73b32e985cc57ba302125e93fb4e9d6c1aecd4f5
sinbadBahri <h.l.kh7990@gmail.com>
2024-01-09 11:11:30 +0330

    Function get_image_url added; Now with this function we can collect a defined number of Image URLs from www.reddit.com You can also define which subreddit hashtag you want to target :)

--- src/collector.py
+++ src/collector.py
@@ -4,10 +4,25 @@ import praw as _praw
 
 _dotenv.load_dotenv()
 
-def create_reddit_client():
+def _create_reddit_client():
     client = _praw.Reddit(
         client_id=_os.environ["CLIENT_ID"],
         client_secret=_os.environ["CLIENT_SECRET"],
         user_agent=_os.environ["USER_AGENT"]
     )
     return client
+
+
+def _get_image_url(client: _praw.Reddit, subreddit_name: str, limit: int):
+    hot_memes = client.subreddit(subreddit_name).hot(limit=limit)
+    image_urls = list()
+    for post in hot_memes:
+        image_urls.append(post.url)
+    
+    return image_urls
+
+
+if __name__ == '__main__':
+    client = _create_reddit_client()
+    urls = _get_image_url(client=client, subreddit_name="memes", limit=10)
+    print(urls)
