e888c6745c6f8dc0ca532c1f571cc155c4a8cac5
sinbadBahri <h.l.kh7990@gmail.com>
2024-01-09 12:46:25 +0330

    Now we have a function to create a folder for us

--- src/collector.py
+++ src/collector.py
@@ -2,8 +2,10 @@ import os as _os
 import dotenv as _dotenv
 import praw as _praw
 import urllib.parse as _parse
+import requests as _requests
 
 _dotenv.load_dotenv()
+
 
 def _create_reddit_client():
     client = _praw.Reddit(
@@ -24,7 +26,7 @@ def _is_image(post):
         return False
 
 
-def _get_image_url(client: _praw.Reddit, subreddit_name: str, limit: int):
+def _get_image_urls(client: _praw.Reddit, subreddit_name: str, limit: int):
     hot_memes = client.subreddit(subreddit_name).hot(limit=limit)
     image_urls = list()
     for post in hot_memes:
@@ -37,6 +39,46 @@ def _get_image_url(client: _praw.Reddit, subreddit_name: str, limit: int):
 def _get_image_name(image_url: str) -> str:
     image_name = _parse.urlparse(image_url)
     return _os.path.basename(image_name)
+
+
+def _create_folder(folder_name: str):
+    """
+    If the folder does not exist, then the function creates
+    a folder using the given name. 
+    """
+    try:
+        _os.mkdir(folder_name)
+    except OSError:
+        print("OOPS; Something went wrong !!")
+    else:
+        print("Folder created")
+
+
+def _download_image(folder_name: str, raw_response, image_name: str):
+    pass
+
+
+# def _collect_memes(subreddit_name: str, limit: int=20):
+#     """
+#     Collect the images from the URLs and stores them into
+#     the folders, named after their subreddits
+#     """
+#     client = _create_reddit_client()
+#     image_urls = _get_image_urls(
+#         client=client,
+#         subreddit_name=subreddit_name,
+#         limit=limit
+#     )
+
+#     for image_url in image_urls:
+#         image_name = _get_image_name(image_url=image_url)
+#         response = _requests.get(image_url, stream=True)
+
+#         if response.status_code == 200:
+#             response.raw.decode_content = True
+#             _download_image()
+
+
 
 
 if __name__ == '__main__':
